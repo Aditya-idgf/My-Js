5f2587d6576c122be959d2f32deb3d7b74dad3b4
Aditya <108931178+Aditya-idgf@users.noreply.github.com>
2024-12-17 21:43:01 +0530

    Add files via upload

---
+++ basics/datatypes.js
@@ -0,0 +1,11 @@
+let name = 'Aditya' //string 
+let age = 12 //number
+// bigint datatype is used for integer with larger values
+let lier = true //boolean
+
+// null standalone value whose type is object
+// objects are another type of datatypes
+// undefined when a value is not defined for example : let myName; // here i dont give myName any value
+
+console.log(typeof null); //this will return object 
+console.log(typeof undefined); //this will return undefined
