3080f905c2db682f388236bc3fe77fea48955b13
Aditya <108931178+Aditya-idgf@users.noreply.github.com>
2024-12-17 13:21:04 +0000

    var commit

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const acc_id = 12346
+var acc_pass = "YEs" //dont ise var
+let acc_mail = "Aditua.c@gmail.com" //use this instead of var
+city = "wy??"
+let acc_val;
+
+// acc_id = 13
+
+console.log(acc_id);
+console.log(acc_mail);
+console.log(acc_pass);
+console.log(city);
