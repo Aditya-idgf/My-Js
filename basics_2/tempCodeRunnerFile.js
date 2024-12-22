a9f384c3328580dbcc514b34f5374c0227110212
Aditya <Aditya.s.chavan247@gmail.com>
2024-12-22 20:21:03 +0530

    Update tempCodeRunnerFile.js

--- basics_2/tempCodeRunnerFile.js
+++ basics_2/tempCodeRunnerFile.js
@@ -1,42 +1,66 @@
-// Singleton objects are created using constructors and are unique.
-// Object.create() can also be used to create singleton objects.
- 
-// Literal objects are not unique and can have copies.
-const obj = {
-    name: "chottaNigga",
-    full_name: "chottaNigga Nandedkar",
-    age: 69,
-    city: "Nanded",
+// functions are created in 3 ways :
+function f1(){
+    console.log("Hello world 1");
+}
+f1()
 
-    // Function within the object
-    anotherFunctionInObject() {
-        console.log(`${this.name} lives in ${this.city}`);
+let f2 = function(){
+    console.log("Hello world 2");
+}
+f2()
+
+let f3 = () =>{
+    console.log("Hello world 3");
+}
+f3()
+
+// Passing args :
+function Sum(n1 , n2){
+    console.log(n1 + n2);
+}
+let s = Sum(10 , 20)
+console.log(s); // since function aint returning anything then this says undefined
+
+// returns values
+function userLogInMessage (userName = 'GodZigga') { // here 'GodZigga' is a default value that means if undef is passed in the function then we can have a defualt instead of undef
+    if (userName === undefined) {
+        console.log(`Invalid user name.`);
+        return
     }
-};
+    return `${userName} has joined the server.`;
+}
+userLogInMessage('KingCruly') // this does anything cuz the returned value isnt being stored anywhere 
+let ret_val = userLogInMessage('KingCruly') // here we get this return value  
+console.log(ret_val);
 
-console.log(obj.name);
-console.log(obj["full_name"]); // Accessing properties using bracket notation
+let new_ret = userLogInMessage() // in this case as nothing is being passed to the function it will just get undefined value 
+console.log(new_ret); // output : GodZigga has joined the server.
 
-obj.name = 'bigNigga'; // Updating object properties
-console.log(obj.name);
+let user_vals = {
+    userNames : "Aditya",
+    userAmount : 199
+}
 
-obj.anotherFunctionInObject();
+let user_details = (AnyObject) => {
+    console.log(`Username is ${AnyObject.userNames} & Useramount is ${AnyObject.userAmount}.`); 
+    console.log(`Username is ${AnyObject.userName} & Useramount is ${AnyObject.userAmount}.`);  // here property name : AnyObject.userName is wrong
+        // while passing a object in function if property names are not correctly given then it we set that value as undefined 
+}
+user_details(user_vals)
+user_details({ // like this we can directly create a new object in function call 
+    userNames : "Zippy",
+    userAmount : 999 
+})
 
-Object.freeze(obj); // Makes the object immutable
-obj.city = "Pune"; // This will not change the city property due to freeze
-console.log(obj);
-
-// Symbol datatype example
-const sym = Symbol('MyKey1');
-
-const s_obj = {
-    sym: "KeyValue1", // This key has a type of string
-    [sym]: "KeyValue2", // This key has a type of Symbol
-
-    // Function within the object
-    functionInObject() {
-        console.log("This is a function in the object");
-    }
-};
-
-s_obj.functionInObject(); // Function call
+let changeName = (obj) => {
+    obj.name = "Rahul" // if u change the passed in object , 
+                // then the real object gets changed as well{happens the same of arrays}
+    return obj
+}
+let obj1 = {
+    name : "Aditya",
+    runing : 1902
+}
+let obj2 = changeName(obj1)
+console.log(obj1);
+console.log(obj2);
