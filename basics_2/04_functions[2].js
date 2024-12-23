2a92248bd61bfd131ed85242977d60900653a3d8
Aditya <Aditya.s.chavan247@gmail.com>
2024-12-23 19:25:59 +0530

    commit 04.functions[2].js

---
+++ basics_2/04_functions[2].js
@@ -0,0 +1,13 @@
+let obj1 = {
+    username : 'Adiiii',
+    tureAccess : true,
+    greetings : function greet(){
+        console.log(`${this.username} has joined the server.`); // this keyword is used to access parameters for object in current context
+        console.log(this);  // here obj1 is the object in current context
+    }}
+
+obj1.greetings()
+obj1.username = 'Saimansays'
+obj1.greetings() // by using obj1 object here we set our current context object to it 
+// if we remove these lines then these lines then {this} will have a empty object to take as a current context 
+console.log(this);
