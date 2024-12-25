c7a0c12c1e844f85c602d62a2b89f1c6685abb2d
Aditya <Aditya.s.chavan247@gmail.com>
2024-12-25 23:09:57 +0530

    Update 05_controlflow.js

--- basics_2/05_controlflow.js
+++ basics_2/05_controlflow.js
@@ -33,3 +33,33 @@ if(1 < "1"){ // comparison btw diff datatypes will result in False
     console.log('No');
 }
     
+// Switch cases in Js
+let operator = "Admin"
+switch (operator) {
+    case ("Admin"):
+        console.log("Admin has joined the server.");
+        break;
+    case ("Co-Admin"):
+        console.log("Co - admin has joined the server.");
+        break;
+    case ("Worker"):
+        console.log("Worker has joined the server.");
+        break;
+
+    default:
+        console.log("Unknow entity joined the server");
+        break;
+}
+
+// null coalescing operator {??} - used to set default values for any values that might get a null or undef value
+let n1; 
+n1 = 5 ?? 10; // since 5 is a valid value then it will take in 5 as its initial value
+console.log(n1);
+
+n1 = null ?? 10 // null is invalid value so it will pass 10 as a defualt value to that variable
+console.log(n1);    
+
+// ternary operator is diff from this operator
+
+let myAge = 19
+myAge >= 18 ? console.log("I can drive") : console.log("I cannot drive");
