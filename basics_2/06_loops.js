e40ab70f753b4b8ac3365732c688eb0288460618
Aditya <Aditya.s.chavan247@gmail.com>
2024-12-26 23:28:19 +0530

    Update 06_loops.js

--- basics_2/06_loops.js
+++ basics_2/06_loops.js
@@ -37,35 +37,3 @@ do {
     // console.log(arr[i]);
     i = i + 2; // Skips every second element
 } while (i < arr.length);
-
-// For of loop
-let str = "Javascript W"
-for (const itrator of str) { // this loop itrates over given entity {string, object, array} taking its indiviual value as its itrator value
-    // console.log(itrator); // prints individual character of the string str
-}
-
-// we have maps in js as : 
-let map = new Map();
-map.set("IND" , "India");
-map.set("USA" , "America");
-map.set("JPN" , "Japan");
-map.set("PAK" , "Pakistan");
-// console.log(map);
-map.set("PAK","Semi-Bangladesh") // Keys are unique in map and will only update its value
-// console.log("Changed Map : " ,map);
-
-// we use for of loop to itrate in maps
-for (const itr of map) {
-    // console.log(itr); // this will print values in array of key value pair {[ 'IND', 'India' ]}
-}
-
-// we do this for better itration
-for (const [key,val] of map) {
-    // console.log(`${key} : ${val}`); // this will split the array from erlier in key and value and print them individually
-}
-
-// For in loop
-const names = "Aditya"
-for (const itr in names) {  
-    // console.log(names[itr]); // similar to simple for loop but the structure is diff
-}
