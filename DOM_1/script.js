86e725e9848ab831e39f29ce5d454106f4545621
Aditya <Aditya.s.chavan247@gmail.com>
2024-12-30 19:50:13 +0530

    Update script.js

--- DOM_1/script.js
+++ DOM_1/script.js
@@ -16,3 +16,35 @@ console.log(upcode.innerText) // output : Heading of page
                 // this will show text that is visible on the screen
 console.log(upcode.textContent) // output : Heading of page "text in span"
                 // this will return all the text inside the tag, same as innerHTML but without tags
+
+// Query selector
+console.log(document.querySelector('h2')) // returns the first query with the value passed
+console.log(document.querySelector('#heading')) // here we passed it as we have query as we do in CSS
+console.log(document.querySelector('p[id = "para"]')) // select element with that attribute
+
+let tempvar = document.querySelector('ul')
+console.log(tempvar.querySelector('li')) // this will get only the first element of the list
+tempvar.querySelector('li').style.color = 'cyan'; // will update for first element of the list
+
+let newVar = document.querySelectorAll('li') // this returns a value that is similar to array but not a complete array {called a NodeList}
+// newVar.style.color = 'cyan'; // will not work since we can update for entire NodeList, only for individual element of that NodeList
+newVar[2].style.color = 'pink'; // by providing index we can access indivial element from NodeList
+
+// since NodeList is kind of array so we have some functions in it :
+newVar.forEach((item) =>{
+    item.style.backgroundColor = '#1B2452'
+    item.style.padding = '10px'
+})
+
+// similar to how we access element with id we can access with class
+let val = document.getElementsByClassName('list-item') // this value return {HTML Collection}
+console.log(val) 
+
+// since both these values are not arrays so to convert them into arrays we can do :
+val = Array.from(val) // from methods converts it to array
+console.log(val) 
+
+val.forEach((item) =>{ // by converting them to arrays we can change values from HTML collection
+    item.style.backgroundColor = '#F57C00'
+    item.style.padding = '10px'
+})
